d77359785e5cccbb97da3ceae547ebcc03404c39
Chess Luo <luoqixin@msu.edu>
2018-03-08 04:11:50 -0500

    connect add game page

--- src/pages/add-game/add-game.ts
+++ src/pages/add-game/add-game.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AddGamePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Court } from '../../types';
+import UserProvider from '../../providers/user/user';
+import TeamProvider from '../../providers/team/team';
+import { Team, User } from '../../types';
 
 @IonicPage()
 @Component({
@@ -14,12 +11,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'add-game.html',
 })
 export class AddGamePage {
+  user: User;
+  court: Court;
+  hour: string;
+  opponent: Team;
+  queryList: Array<Team> = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public userProvider: UserProvider,
+    public teamProvider: TeamProvider,
+  ) {
+    this.court = this.navParams.get('court');
+    this.hour = this.navParams.get('hour');
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad AddGamePage');
+    this.userProvider.getCurrentUser().then((user: User) => this.user = user);
+  }
+
+  getItems(searchbar) {
+    const key = searchbar.srcElement.value;
+    if (!key) {
+      this.queryList = [];
+      return;
+    }
+
+    this.teamProvider.getAllTeams().then((teams: Array<Team>) => {
+      this.queryList = teams.filter((team: Team) => {
+        if (team.teamName && key) {
+          if (team.teamName.toLowerCase().indexOf(key.toLowerCase()) > -1) {
+            return true;
+          }
+          return false;
+        }
+      });
+    });
+  }
+
+  setOpponent(team: Team) {
+    this.opponent = team;
   }
 
 }
