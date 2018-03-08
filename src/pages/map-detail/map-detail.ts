6ee70a1d0ae02864248dfccf4e64b9017da8b524
Chess Luo <luoqixin@msu.edu>
2018-03-08 04:12:21 -0500

    connect map detail page

--- src/pages/map-detail/map-detail.ts
+++ src/pages/map-detail/map-detail.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import CourtProvider from '../../providers/court/court';
+import { CourtDetail } from '../../types';
 
 @IonicPage()
 @Component({
@@ -7,15 +9,34 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'map-detail.html',
 })
 export class MapDetailPage {
+  court: CourtDetail;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public viewCtrl: ViewController,
-  ) {}
+    public courtProvider: CourtProvider,
+  ) {
+  }
+
+  ngOnInit() {
+    const id = this.navParams.get('id');
+    this.courtProvider.getCourtById(id)
+    .then((court: CourtDetail) => {
+      this.court = court;
+    });
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  goback() {
+    this.navCtrl.pop();
+  }
+
+  openSchedule() {
+    this.navCtrl.push('SchedulePage', { court: this.court });
   }
 
 }
