59a09148cc8c20254a8361758640679a4460628d
Chess Luo <luoqixin@msu.edu>
2018-03-08 04:12:47 -0500

    connect schedule page

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import moment from 'moment';
+import { Challenge, CourtDetail } from '../../types';
 
-/**
- * Generated class for the SchedulePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+type Schedule = {
+  hour: string,
+  teams: Array<string>,
+};
 
 @IonicPage()
 @Component({
@@ -14,8 +14,41 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'schedule.html',
 })
 export class SchedulePage {
-
+  court: CourtDetail;
+  schedules: Array<Schedule> = [];
+  hours: Array<string> = ['1PM', '2PM', '3PM', '4PM', '5PM', '6PM'];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.court = this.navParams.get('court');
+    this.court.challenges.forEach((challenge: Challenge) => {
+      const hour = moment(challenge.gameTime).hour();
+      const schedule: Schedule = {
+        hour: hour >= 12 ? (hour - 12) + 'PM' : hour + 'AM',
+        teams: challenge.teams.map(t => t.teamName),
+      };
+      this.schedules.push(schedule);
+    });
+  }
+
+  isHourOpen(hour: string): boolean {
+    const find = this.schedules.find((s: Schedule) => s.hour === hour);
+    if (find) {
+      return false;
+    }
+    return true;
+  }
+
+  findTeams(hour: string): Array<string> {
+    return this.schedules.find((s: Schedule) => s.hour === hour).teams;
+  }
+
+  goback() {
+    this.navCtrl.pop();
+  }
+
+  addGame(hour: string) {
+    if (this.isHourOpen(hour)) {
+      this.navCtrl.push('AddGamePage', { court: { id: this.court.id, courtName: this.court.courtName }, hour });
+    }
   }
 
   ionViewDidLoad() {
